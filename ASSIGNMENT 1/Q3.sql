7197cf99ce72fc838c28d9170b42027237b216a6
Varun Kumbhare <varun.s.kumbhare@gmail.com>
2026-08-28 22:42:35 +0530

    Create Q3.sql creation of table dup_countries like countries
    use of LIKE

---
+++ ASSIGNMENT 1/Q3.sql
@@ -0,0 +1,11 @@
+mysql> create table dup_countries LIKE countries;
+Query OK, 0 rows affected (0.02 sec)
+
+mysql> show tables;
++-----------------------+
+| Tables_in_assignment1 |
++-----------------------+
+| countries             |
+| dup_countries         |
++-----------------------+
+2 rows in set (0.00 sec)
