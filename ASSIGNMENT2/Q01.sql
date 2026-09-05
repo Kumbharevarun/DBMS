81cedb51401b2909c2bd6745a776a35fa994f017
Varun Kumbhare <varun.s.kumbhare@gmail.com>
2026-09-05 17:32:08 +0530

    using select command to view data using ALIAS

---
+++ ASSIGNMENT2/Q01.sql
@@ -0,0 +1,5 @@
+mysql> select first_name AS 'First name',
+    -> last_name AS 'Last Name'
+    -> from employees;
+Empty set (0.00 sec)
+--- since we havent inserted any data yet-------------------
